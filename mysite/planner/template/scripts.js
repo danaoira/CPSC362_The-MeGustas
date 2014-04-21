aa9b815faed2389fa13ea624720d4e41f8b3fa75
Dana <dana.toribio@codazen.com>
2014-04-20 19:00:04 -0700

    Added scripts.js, updated calendar/index/search. jQuery use for resizes in progress.

---
+++ mysite/planner/template/scripts.js
@@ -0,0 +1,18 @@
+// Name: Dana Toribio
+// Date: 4/15/14
+// Filename: scripts.js
+// Description: This is the scripts.js file which holds all the
+// 		JavaScript/jQuery code to help with assisting in formatting
+// 		TitanPlanner in areas that CSS cannot sufficiently format.
+
+$(document).ready(function() {
+	// formats the height of index.html's iframes to fill the window
+	$("iframe").height($(window).height());
+	//$(".time-slot").width($(window).width(timeslot-width);
+});
+
+//var timeslot-width = ($(window).width() - 45px) / 7;
+
+function iframeResize() {
+	var 
+}
